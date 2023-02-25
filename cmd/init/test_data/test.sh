2c34aeba103389a2d2bd8ec8ed0e06770a1014f0
monserrat <monserrat@selefra.io>
2023-02-25 20:25:16 +0800

    add script for help test selefra init command

---
+++ cmd/init/test_data/test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#######################################################################################################################
+#                                                                                                                     #
+#                              This script helps you test interactive programs                                        #
+#                                                                                                                     #
+#                                                                                                                     #
+#                                                                                                   Version: 0.0.1    #
+#                                                                                                                     #
+#######################################################################################################################
+
+# for command `selefra init`
+cd ../../../
+go build
+rm -rf ./test
+mkdir test
+mv selefra.exe ./test
+cd test
+./selefra.exe init --force
+
