b034b0e2e44a1bffbaae0d55652c188d9da2b845
monserrat <monserrat@selefra.io>
2023-02-25 16:58:06 +0800

    Update template.go

--- pkg/utils/template.go
+++ pkg/utils/template.go
@@ -1,11 +1,21 @@
 package utils
 
 import (
+	"fmt"
 	"strings"
 	"text/template"
 )
 
-func RenderingTemplate[T any](templateName, templateString string, data T) (string, error) {
+func RenderingTemplate[T any](templateName, templateString string, data T) (s string, err error) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("rendering template error: %v", r)
+		}
+	}()
+
+	// prevent <no value>
+	//parse, err := template.New(templateName).Option("missingkey=zero").Parse(templateString)
 	parse, err := template.New(templateName).Parse(templateString)
 	if err != nil {
 		return "", err
@@ -15,5 +25,6 @@ func RenderingTemplate[T any](templateName, templateString string, data T) (stri
 	if err != nil {
 		return "", err
 	}
+	//return strings.ReplaceAll(builder.String(), "<no value>", ""), nil
 	return builder.String(), nil
 }
