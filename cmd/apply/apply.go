55a679e8c9572be51923afacaa98b5c52d7a3ca7
monserrat <monserrat@selefra.io>
2023-02-25 17:43:27 +0800

    Update apply.go

--- cmd/apply/apply.go
+++ cmd/apply/apply.go
@@ -1,9 +1,14 @@
 package apply
 
 import (
-	"github.com/selefra/selefra/config"
+	"context"
+	"github.com/selefra/selefra-provider-sdk/env"
+	"github.com/selefra/selefra-provider-sdk/provider/schema"
+	"github.com/selefra/selefra/cli_ui"
 	"github.com/selefra/selefra/global"
-	"github.com/selefra/selefra/pkg/cli_runtime"
+	"github.com/selefra/selefra/pkg/message"
+	"github.com/selefra/selefra/pkg/modules/executors"
+	"github.com/selefra/selefra/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +18,13 @@ func NewApplyCmd() *cobra.Command {
 		Short:            "Analyze infrastructure",
 		Long:             "Analyze infrastructure",
 		PersistentPreRun: global.DefaultWrappedInit(),
-		RunE:             apply,
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			projectWorkspace := "./test_data/test_query_module"
+			downloadWorkspace := "./test_download"
+
+			return Apply(cmd.Context(), projectWorkspace, downloadWorkspace)
+		},
 	}
 
 	cmd.SetHelpFunc(cmd.HelpFunc())
@@ -22,16 +33,31 @@ func NewApplyCmd() *cobra.Command {
 
 // ------------------------------------------------- --------------------------------------------------------------------
 
-func apply(cmd *cobra.Command, args []string) error {
+func Apply(ctx context.Context, projectWorkspace, downloadWorkspace string) error {
 
-	cli_runtime.Init("./")
-
-	projectWorkspace := "./"
-	downloadWorkspace, _ := config.GetDefaultDownloadCacheDirectory()
-	NewApplyCommandExecutor(&ApplyCommandExecutorOptions{
-		ProjectWorkspace:  projectWorkspace,
-		DownloadWorkspace: downloadWorkspace,
-	}).Run(cmd.Context())
+	messageChannel := message.NewChannel[*schema.Diagnostics](func(index int, message *schema.Diagnostics) {
+		if utils.IsNotEmpty(message) {
+			_ = cli_ui.PrintDiagnostics(message)
+		}
+	})
+	d := executors.NewProjectLocalLifeCycleExecutor(&executors.ProjectLocalLifeCycleExecutorOptions{
+		ProjectWorkspace:                     projectWorkspace,
+		DownloadWorkspace:                    downloadWorkspace,
+		MessageChannel:                       messageChannel,
+		ProjectLifeCycleStep:                 executors.ProjectLifeCycleStepQuery,
+		FetchStep:                            executors.FetchStepFetch,
+		ProjectCloudLifeCycleExecutorOptions: nil,
+		DSN:                                  env.GetDatabaseDsn(),
+		FetchWorkerNum:                       1,
+		QueryWorkerNum:                       1,
+	}).Execute(context.Background())
+	messageChannel.ReceiverWait()
+	if utils.IsNotEmpty(d) {
+		_ = cli_ui.PrintDiagnostics(d)
+		cli_ui.Errorln("Apply failed")
+	} else {
+		cli_ui.Errorln("Apply success")
+	}
 	return nil
 }
 
