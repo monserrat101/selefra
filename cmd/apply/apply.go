3c81b3431131158a2bf6cdfba0da82bfbeed5ba2
monserrat <monserrat@selefra.io>
2023-02-27 19:09:41 +0800

    Update apply.go

--- cmd/apply/apply.go
+++ cmd/apply/apply.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/selefra/selefra-provider-sdk/provider/schema"
 	"github.com/selefra/selefra/cli_ui"
+	"github.com/selefra/selefra/config"
 	"github.com/selefra/selefra/global"
 	"github.com/selefra/selefra/pkg/message"
 	"github.com/selefra/selefra/pkg/modules/executors"
@@ -19,8 +20,11 @@ func NewApplyCmd() *cobra.Command {
 		PersistentPreRun: global.DefaultWrappedInit(),
 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			projectWorkspace := "./test_data/test_query_module"
-			downloadWorkspace := "./test_download"
+			//projectWorkspace := "./test_data/test_query_module"
+			//downloadWorkspace := "./test_download"
+
+			projectWorkspace := "./"
+			downloadWorkspace, _ := config.GetDefaultDownloadCacheDirectory()
 
 			return Apply(cmd.Context(), projectWorkspace, downloadWorkspace)
 		},
