947ed25c4b2c079f8b444ad53b63dea7c5b0cb6a
monserrat <monserrat@selefra.io>
2023-03-21 17:13:09 +0800

    Update pgstorage.go

--- pkg/storage/pgstorage/pgstorage.go
+++ pkg/storage/pgstorage/pgstorage.go
@@ -112,6 +112,7 @@ func DefaultPostgreSQL(downloadWorkspace string, messageChannel *message.Channel
 	isRunSuccess := true
 
 	runOCIPostgreSQLOnce.Do(func() {
+		messageChannel.Send(schema.NewDiagnostics().Add("Use built-in PostgreSQL database..."))
 		downloader := oci.NewPostgreSQLDownloader(&oci.PostgreSQLDownloaderOptions{
 			MessageChannel:    messageChannel.MakeChildChannel(),
 			DownloadDirectory: downloadWorkspace,
